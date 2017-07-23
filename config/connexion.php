6d97688fc680b6b642a3ee7a1fcf5014bf86dabb
elo <eloisemecozzi@hotmail.fr>
2017-07-23 20:46:30 +0200

    placement des fichiers dans les dossiers

--- config/connexion.php
+++ config/connexion.php
@@ -1,20 +1,4 @@
 <?php
-// Connexion à la base de données
-// $serverName = 'localhost';
-// $dbname = 'restaurant';
-// $login = 'nico';
-// $mdp = 'menu';
-//
-//
-// try
-// {
-//     $bdd = new PDO("mysql:host=$serverName;dbname=$dbname;charset=utf8", $login, $mdp);
-//     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// }
-// catch(Exception $e)
-// {
-//         die('Erreur : '.$e->getMessage());
-// }
 
 $servername = "localhost";
 $username = "nico";
@@ -22,9 +6,7 @@ $password = "menu";
 
 try {
     $bdd = new PDO("mysql:host=$servername;dbname=restaurant", $username, $password, array (PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
-    // set the PDO error mode to exception
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "Connected successfully";
     }
 catch(PDOException $e)
     {
